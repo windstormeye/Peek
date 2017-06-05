d24d573361542324fbdc143ea5c9a759b832b58a
pjpjpj <877302410@qq.com>
2017-06-06 00:03:45 +0800

    完善 左侧菜单栏（完善丝滑拖动、添加蒙版及相关点击事件）

--- Peek/LeftSliderView.m
+++ Peek/LeftSliderView.m
@@ -18,7 +18,6 @@
 
 - (void)initView {
     self.frame = CGRectMake(-SCREEN_WIDTH * 0.6, 0, SCREEN_WIDTH * 0.6, SCREEN_HEIGHT);
-    
     self.backgroundColor = [UIColor clearColor];
     
     // 开启高斯模糊
@@ -28,43 +27,6 @@
     [self addSubview:effectView];
 }
 
-- (void)closeView:(UISwipeGestureRecognizer *)ges {
-//    [UIView animateWithDuration:0.3 animations:^{
-//        CGRect leftframe = self.frame;
-//        leftframe.origin.x = -SCREEN_WIDTH * 0.6;
-//        self.frame = leftframe;
-//    }];
-    
-    CGPoint p = [ges locationInView:self];
-    NSLog(@"%@", NSStringFromCGPoint(p));
-    CGRect frame = self.frame;
-
-    if (frame.origin.x == 0) {
-        return;
-    }
-    // 更改adView的x值.当滑动距离超过view宽度时，则把x一直设置为0
-    if (p.x > SCREEN_WIDTH * 0.6) {
-        frame.origin.x = 0;
-    } else {
-        CGRect frame = self.frame;
-        frame.origin.x = p.x - SCREEN_WIDTH * 0.6;
-    }
-    
-//    if (frame.origin.x == 0) {
-//        frame.origin.x = 0;
-//    }
-//    
-//    if (p.x > SCREEN_WIDTH * 0.6) {
-//        frame.origin.x = 0;
-//    }
-    
-//    if (p.x > SCREEN_WIDTH * 0.3) {
-//        frame.origin.x = 0;
-//    } else {
-//        frame.origin.x = -SCREEN_WIDTH * 0.6;;
-//    }
-    self.frame = frame;
-}
 
 
 @end
--- Peek/ViewController.m
+++ Peek/ViewController.m
@@ -11,8 +11,7 @@
 
 @interface ViewController () <UIImagePickerControllerDelegate,UINavigationControllerDelegate>
 
-@property(nonatomic,strong)UIView *rightView;
-@property(nonatomic,strong)UIView *rightBGView;
+@property (nonatomic, strong) UIView *BGView;
 @property (nonatomic, strong) UIPanGestureRecognizer *panGesture;
 
 @property (nonatomic,strong) UIImagePickerController *imagePicker;
@@ -52,6 +51,15 @@
     effectView.frame = CGRectMake(0, 0, bgImgView.frame.size.width, bgImgView.frame.size.height);
     [bgImgView addSubview:effectView];
     
+    self.BGView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+    [self.view addSubview:self.BGView];
+    self.BGView.backgroundColor = [UIColor blackColor];
+    self.BGView.hidden = YES;
+    self.BGView.alpha = 0;
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(Actiondo)];
+    
+    [self.BGView addGestureRecognizer:tapGesture];
+    
     [self.leftBarButton setImage:[UIImage imageNamed:@"汉堡线"] forState:0];
     [self.leftBarButton addTarget:self action:@selector(moreAction) forControlEvents:1<<6];
     [self.rightBarButton setImage:[UIImage imageNamed:@"朋友"] forState:0];
@@ -60,6 +68,7 @@
     
     _leftView = [[LeftSliderView alloc] init];
     [self.view addSubview:_leftView];
+    [self.view bringSubviewToFront:_leftView];
     
     UIScreenEdgePanGestureRecognizer *edgePan = [[UIScreenEdgePanGestureRecognizer alloc]initWithTarget:self action:@selector(showLeftAd:)];
     edgePan.edges = UIRectEdgeLeft;
@@ -114,6 +123,18 @@
         return _imagePicker;
 }
 
+-(void)Actiondo{
+    [UIView animateWithDuration:0.25 animations:^{
+        CGRect frame = _leftView.frame;
+        frame.origin.x = -SCREEN_WIDTH * 0.6;
+        _panGesture.enabled = NO;
+        self.BGView.alpha = 0;
+        self.BGView.hidden = YES;
+        _leftView.frame = frame;
+    }];
+}
+
+// 左侧菜单栏滑动退出
 - (void)panGesture:(UIPanGestureRecognizer *)ges {
     CGPoint p = [ges translationInView:self.view];
     NSLog(@"%@", NSStringFromCGPoint(p));
@@ -123,35 +144,36 @@
     CGPoint velocity = [ges velocityInView:self.view];
     if(velocity.x>0) {
         NSLog(@"zzz");
+        frame.origin.x = 0;
         return;
     } else {
-        NSLog(@"rrr");
-    }
-    
-    frame.origin.x = p.x;
-
-    _leftView.frame = frame;
-    
-    
-    if (ges.state == UIGestureRecognizerStateEnded || ges.state == UIGestureRecognizerStateCancelled) {
-        // 判断当前广告视图在屏幕上显示是否超过一半
-        if (CGRectContainsPoint(self.view.frame, _leftView.center)) {
-            // 如果超过,那么完全展示出来
-            frame.origin.x = 0;
-        }else{
-            // 如果没有,隐藏
-            frame.origin.x = -SCREEN_WIDTH * 0.6;
-            _panGesture.enabled = NO;
+        frame.origin.x = p.x;
+        
+        _leftView.frame = frame;
+        
+        if (ges.state == UIGestureRecognizerStateEnded || ges.state == UIGestureRecognizerStateCancelled) {
+            // 判断当前广告视图在屏幕上显示是否超过一半
+            if (CGRectContainsPoint(self.view.frame, _leftView.center)) {
+                // 如果超过,那么完全展示出来
+                frame.origin.x = 0;
+            }else{
+                // 如果没有,隐藏
+                frame.origin.x = -SCREEN_WIDTH * 0.6;
+                _panGesture.enabled = NO;
+                [UIView animateWithDuration:0.25 animations:^{
+                    self.BGView.alpha = 0;
+                    self.BGView.hidden = YES;
+                }];
+            }
+            [UIView animateWithDuration:0.25 animations:^{
+                _leftView.frame = frame;
+            }];
         }
-        [UIView animateWithDuration:0.25 animations:^{
-            _leftView.frame = frame;
-        }];
     }
 }
 
 // 开启左侧滑功能
 - (void)showLeftAd:(UIScreenEdgePanGestureRecognizer *)ges {
-    
     CGPoint p = [ges locationInView:self.view];
     NSLog(@"%@", NSStringFromCGPoint(p));
     CGRect frame = _leftView.frame;
@@ -174,127 +196,32 @@
             // 如果超过,那么完全展示出来
             frame.origin.x = 0;
             _panGesture.enabled = YES;
+            self.BGView.alpha = 0.15;
+            self.BGView.hidden = NO;
         }else{
             // 如果没有,隐藏
             frame.origin.x = -SCREEN_WIDTH * 0.6;
         }
         [UIView animateWithDuration:0.25 animations:^{
             _leftView.frame = frame;
+            
         }];
     }
-}
-
-// 开启右侧滑功能
-- (void)showRightAd:(UIScreenEdgePanGestureRecognizer *)ges {
-    CGPoint p = [ges locationInView:self.view];
-    NSLog(@"%@", NSStringFromCGPoint(p));
-    CGRect frame = self.rightView.frame;
-    
-    // 如果已经划出view
-    if (frame.origin.x == SCREEN_WIDTH * 0.4) {
-        return;
-    }
-    
-    // 更改adView的x值.当滑动距离超过view宽度时，则把x一直设置为0
-    if (p.x < SCREEN_WIDTH * 0.4) {
-        frame.origin.x = SCREEN_WIDTH * 0.4;
-    } else {
-        frame.origin.x = p.x;
-    }
-    self.rightView.frame = frame;
-    
-    if (ges.state == UIGestureRecognizerStateEnded || ges.state == UIGestureRecognizerStateCancelled) {
-        // 判断当前广告视图在屏幕上显示是否超过一半
-        if (CGRectContainsPoint(self.view.frame, self.rightView.center)) {
-            // 如果超过,那么完全展示出来
-            frame.origin.x = SCREEN_WIDTH * 0.4;
-        }else{
-            // 如果没有,隐藏
-            frame.origin.x = SCREEN_WIDTH;
-        }
-        [UIView animateWithDuration:0.25 animations:^{
-            self.rightView.frame = frame;
-        }];
-    }
-}
-// 初始化右侧视图
-- (void)setupRightView {
-    self.rightBGView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)];
-    self.rightBGView.backgroundColor = [UIColor blackColor];
-    self.rightBGView.alpha = 0;
-    self.rightBGView.hidden = YES;
-    [self.view addSubview:self.rightBGView];
-    
-    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH * 0.6, SCREEN_HEIGHT)];
-    // 设置背景颜色
-    [self.view addSubview:view];
-    view.backgroundColor = [UIColor clearColor];
-    
-    // 开启高斯模糊
-    UIBlurEffect *effect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
-    UIVisualEffectView *effectView = [[UIVisualEffectView alloc] initWithEffect:effect];
-    effectView.frame = CGRectMake(0, 0, view.frame.size.width, view.frame.size.height);
-    [view addSubview:effectView];
-    
-    CGRect frame = view.frame;
-    // 将x值改成负的屏幕宽度,原因是因为默认就在屏幕的右边
-    frame.origin.x = SCREEN_WIDTH;
-    // 设置给view
-    view.frame = frame;
-    self.rightView = view;
-    
-    // 添加轻扫手势  -- 滑回
-    UISwipeGestureRecognizer *ges = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(closeView)];
-    ges.direction = UISwipeGestureRecognizerDirectionRight;
-    [self.view addGestureRecognizer:ges];
-}
-
-- (void)closeView {
-    
-    [UIView animateWithDuration:0.3 animations:^{
-        CGRect leftframe = _leftView.frame;
-        CGRect rightframe = self.rightView.frame;
-        // 如果view已经隐藏
-        if (leftframe.origin.x == -SCREEN_WIDTH * 0.6) {
-            if (rightframe.origin.x == SCREEN_WIDTH * 0.4) {
-                rightframe.origin.x = SCREEN_WIDTH;
-                self.rightView.frame = rightframe;
-                return;
-            } else {
-                return;
-            }
-        } else {
-            leftframe.origin.x = -SCREEN_WIDTH * 0.6;
-            _leftView.frame = leftframe;
-        }
-        
-        // 如果view已经隐藏
-        if (rightframe.origin.x == SCREEN_WIDTH) {
-            return;
-        } else {
-            rightframe.origin.x = SCREEN_WIDTH;
-            _leftView.frame = rightframe;
-        }
-    }];
-    self.rightBGView.hidden = YES;
 }
 
 - (void)moreAction {
     [UIView animateWithDuration:0.25 animations:^{
+        _panGesture.enabled = YES;
         CGRect frame = _leftView.frame;
         frame.origin.x = 0;
         _leftView.frame = frame;
+        self.BGView.alpha = 0.15;
+        self.BGView.hidden = NO;
     }];
 }
 
 - (void)friendAction {
-    [UIView animateWithDuration:0.25 animations:^{
-        CGRect frame = self.rightView.frame;
-        frame.origin.x = SCREEN_WIDTH * 0.4;
-        self.rightView.frame = frame;
-        self.rightBGView.hidden = NO;
-        self.rightBGView.alpha = 0.3;
-    }];
+    
 }
 
 
