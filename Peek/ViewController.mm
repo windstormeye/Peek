21298c7044b0af05a7e8f4f822899c95c9ce1ef5
pjpjpj <877302410@qq.com>
2017-10-08 12:46:32 +0800

    添加 OpenCV 3.3.0框架及.gitignore

--- Peek/ViewController.mm
+++ Peek/ViewController.mm
@@ -16,7 +16,13 @@
 #import <UMSocialCore/UMSocialCore.h>
 #import <UShareUI/UShareUI.h>
 
-@interface ViewController () <UIImagePickerControllerDelegate,UINavigationControllerDelegate, leftHomeViewDelegate>
+#import <opencv2/opencv.hpp>
+#import <opencv2/imgproc/types_c.h>
+#import <opencv2/imgcodecs/ios.h>
+
+@interface ViewController () <UIImagePickerControllerDelegate,UINavigationControllerDelegate, leftHomeViewDelegate>{
+    cv::Mat cvImage;
+}
 
 @property (nonatomic, strong) UIPanGestureRecognizer *panGesture;
 @property (nonatomic, strong) UIScreenEdgePanGestureRecognizer *edgePan;
@@ -84,8 +90,8 @@
     _panGesture.enabled = NO;
     [self.view addGestureRecognizer:_panGesture];
     
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(loginReload:) name:@"loginNo"object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(changeUserDateReload:) name:@"changeAvarta"object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(loginReload:) name:@"loginNo"  object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(changeUserDateReload:) name:@"changeAvarta"  object:nil];
 }
 
 // 用户资料的更新通知
@@ -116,6 +122,24 @@
     [self.view addSubview:img];
     img.image = info[@"UIImagePickerControllerOriginalImage"];
 
+    
+    UIImageToMat(img.image, cvImage);
+    if(!cvImage.empty()){
+        cv::Mat gray;
+        // 将图像转换为灰度显示
+        cv::cvtColor(cvImage,gray,CV_RGB2GRAY);
+        // 应用高斯滤波器去除小的边缘
+        cv::GaussianBlur(gray, gray, cv::Size(5,5), 1.2,1.2);
+        // 计算与画布边缘
+        cv::Mat edges;
+        cv::Canny(gray, edges, 0, 50);
+        // 使用白色填充
+        cvImage.setTo(cv::Scalar::all(225));
+        // 修改边缘颜色
+        cvImage.setTo(cv::Scalar(0,128,255,255),edges);
+        // 将Mat转换为Xcode的UIImageView显示
+        img.image = MatToUIImage(cvImage);
+    }
     
     // 测试代码
     
